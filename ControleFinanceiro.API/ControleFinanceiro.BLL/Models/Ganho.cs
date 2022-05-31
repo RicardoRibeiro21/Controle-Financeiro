cdc417ed1a3073501aec8e1ca674b6f4de301ef0
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2022-05-31 00:07:23 -0300

    Mapemeamento Mes e Cartao.

--- ControleFinanceiro.API/ControleFinanceiro.BLL/Models/Ganho.cs
+++ ControleFinanceiro.API/ControleFinanceiro.BLL/Models/Ganho.cs
@@ -13,6 +13,7 @@ namespace ControleFinanceiro.BLL.Models
         public double Valor { get; set; }
         public int Dia { get; set; }
         public int MesId { get; set; }
+        public Mes Mes { get; set; }
         public int Ano { get; set; }
         public string UsuarioId { get; set; }
         public Usuario Usuario { get; set; }
---
+++ ControleFinanceiro.API/ControleFinanceiro.DAL/Mapeamentos/CartaoMap.cs
@@ -0,0 +1,33 @@
+﻿using ControleFinanceiro.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleFinanceiro.DAL.Mapeamentos
+{
+    public class CartaoMap : IEntityTypeConfiguration<Cartao>
+    {
+        public void Configure(EntityTypeBuilder<Cartao> builder)
+        {
+            // Chave primária
+            builder.HasKey(c => c.CartaoId);
+
+            // Campos
+            builder.Property(c => c.Nome).HasMaxLength(50).IsRequired();
+            builder.Property(c => c.Bandeira).HasMaxLength(15).IsRequired();
+
+            builder.Property(c => c.Numero).HasMaxLength(20).IsRequired();
+            builder.HasIndex(c => c.Numero).IsUnique();
+            builder.Property(c => c.Limite).IsRequired();
+
+            // Relacionamentos
+            builder.HasOne(c => c.Usuario).WithMany(c => c.Cartoes).HasForeignKey(c => c.UsuarioId).IsRequired().OnDelete(DeleteBehavior.NoAction);
+            builder.HasMany(c => c.Despesas).WithOne(c => c.Cartao);
+             
+            // Alterando o nome da tabela na hora da criação
+            builder.ToTable("Categorias");
+        }
+    }
+}
